2a01ef0229d94450230719eedba442d2c096f3ee
Kirill <kirillnechaev3120@gmail.com>
2022-11-17 16:25:56 +0300

    ветка 2 от ветки 1 . удалил все кроме одной функции и функцию plus

--- file1.py
+++ file1.py
@@ -1,27 +1,6 @@
 
 def sum(a,b):
     return a+b
-def sum1(a):
-    return 1+a
-def sum5(a):
-    return a + 5
-def sum2(a):
-    return 2 + a
-def sum3(s):
-
-    return 3 + s
-
-def sum4(a):
-     return 3 + a
-def sub(a,b):
-    return a-b
-
-def sum15(a):
-    return a + 15
-
-s = 'abcdef'
-
-s=s.upper()
-print(s)
-print(s.lower())
-print('tresk')
+')
+def plus(a=10,b):
+    return a + b
