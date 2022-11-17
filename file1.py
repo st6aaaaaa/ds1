5e875391a1028b1e82f81cc32ad354b3ce29641a
Kirill <kirillnechaev3120@gmail.com>
2022-11-17 10:33:30 +0300

    added sum5 , sum4 in file1 branch1_new

--- file1.py
+++ file1.py
@@ -3,11 +3,15 @@ def sum(a,b):
     return a+b
 def sum1(a):
     return 1+a
+def sum5(a):
+    return a + 5
 def sum2(a):
     return 2 + a
 def sum3(s):
     return 3 + s
 
+def sum4(a):
+    return a+ 4
 
 
 s = 'abcdef'
