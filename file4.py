1db530f322501f0b0eef410cbf24c23231830c36
Kirill <kirillnechaev3120@gmail.com>
2022-11-20 11:31:48 +0300

    file5 bs()

--- file4.py
+++ file4.py
@@ -2,6 +2,15 @@ def f4_1():
     return 'f4_1'
 def f4_2():
     return 'f4_2'
+def f4_3():
+    return 'f4_3'
+def br_f4_3():
+    return 'br_f4_3()'
+def f4_4():
+    return 'f4_4'
+def m():
+    return 'm'
+
 
 def br_f4_3():
     return 'br_f4_3()'
